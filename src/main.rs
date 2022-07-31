b5d2e48be1ff2ebc94d1a6c4df9c990bd9f28e52
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-07-30 21:06:42 -0400

    Comment code, implement several functions on Program type including default

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use uuid::Uuid;
 
 use regex::{Match, Regex};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct FunctionDeclaration {
     type_of: String,
     start: usize,
@@ -14,37 +14,141 @@ struct FunctionDeclaration {
     identifier: Identifier,
     params: Params,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Identifier {
     type_of: String,
     start: usize,
     end: usize,
     name: String,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Params {
     number: Vec<f64>,
     string: Vec<String>,
     bool: Vec<bool>,
 }
-
+#[derive(Debug)]
 struct Program {
     type_of: String,
     start: usize,
     end: usize,
     VariableDeclaration: Vec<VariableDeclaration>,
-    ExpressionDeclaration: Vec<ExpressionDeclaration>,
+    FunctionDeclaration: Vec<FunctionDeclaration>,
     ExpressionStatement: Vec<ExpressionStatement>,
 }
-
+#[derive(Debug)]
 struct VariableDeclaration {}
-struct ExpressionDeclaration{}
-struct ExpressionStatement{}
+#[derive(Debug)]
+struct ExpressionStatement {}
 
+impl Program {
+    //eventually the main loop that will go through program string and turn it into AST and nodes, using 
+    //looping
+    fn parse_program(&mut self, program: String) {
+        let mat = Regex::new("(function|const|let|if|for|console.log(\\(*)\\))")
+            .unwrap()
+            .find(&program)
+            .expect("found no program");
+        let string_for_match = program.substring(mat.start(), mat.end());
+        if string_for_match == "const" || string_for_match == "let" {
+        } else if string_for_match == "function" {
+            let function_declaration =
+                Program::match_function_declaration_start_parse("function", program)
+                    .unwrap()
+                    .clone();
+            self.add_function_declaration(function_declaration);
+            println!("{:?}", self)
+        } else if string_for_match == "if"
+            || string_for_match == "for"
+            || string_for_match == "console.log()"
+        {
+        }
+        //This one will check for existing variables to determine if valid expression.
+    }
+    //These three match functions will take the already decided type (func_dec, var_dec, expression_statement and call parser functions, returning the node)
+    fn match_var_declaration_start_parse(type_of_string_for_match: &str, program_string: String) {
+        match type_of_string_for_match {
+            "const" => {
+                // println!("found {}!", type_of_string_for_match)
+            }
+            "let" => {
+                // println!("found {}!", type_of_string_for_match)
+            }
+            _ => {
+                return;
+            }
+        }
+    }
+    fn match_function_declaration_start_parse(
+        type_of_string_for_match: &str,
+        program_string: String,
+    ) -> Result<FunctionDeclaration, String> {
+        let function_declaration: Result<FunctionDeclaration, String> =
+            match type_of_string_for_match {
+                "function" => {
+                    let result = parse_functions(program_string);
+                    Ok(result)
+                }
+                _ => Err("No Function Declarations Found!".to_string()),
+            };
+        function_declaration
+    }
+
+    fn match_expression_statement_start_parse(
+        type_of_string_for_match: &str,
+        program_string: String,
+    ) {
+        match type_of_string_for_match {
+            "if" => {
+                let function = parse_functions(program_string);
+            }
+            "for" => {
+                let function = parse_functions(program_string);
+            }
+            "console.log" => {
+                let function = parse_functions(program_string);
+            }
+            _ => {
+                // println!("{}", "NO EXPRESSION FOUND!");
+            }
+        }
+    }
+    //I think this (and probably some other things) will be better outside of program later, possibly in a trait, since I'll use something similar for parsing function declaration and other statement blocks
+    fn add_function_declaration(&mut self, data_to_add: FunctionDeclaration) {
+        self.FunctionDeclaration.push(data_to_add);
+    }
+    fn add_variable_declaration(&mut self, data_to_add: VariableDeclaration) {
+        self.VariableDeclaration.push(data_to_add);
+    }
+    fn add_expression_statement(&mut self, data_to_add: ExpressionStatement) {
+        self.ExpressionStatement.push(data_to_add);
+    }
+}
+
+impl Default for Program {
+    fn default() -> Program {
+        Program {
+            type_of: "Program".to_string(),
+            start: 0,
+            end: 0,
+            VariableDeclaration: Vec::new(),
+            FunctionDeclaration: Vec::new(),
+            ExpressionStatement: Vec::new(),
+        }
+    }
+}
 fn main() {
     let file_string = read_file_line_by_line("src/test/test.txt");
+
     let trimmed = skip_space(&file_string);
-    parse_functions(&trimmed);
+    let mut program = Program {
+        end: trimmed.len(),
+        ..Default::default()
+    };
+
+    Program::parse_program(&mut program, trimmed);
+
+    // parse_functions(&trimmed);
     // println!("{}", trimmed);
 }
 
@@ -52,11 +156,10 @@ fn create_id() -> Uuid {
     let id = Uuid::new_v4();
     id
 }
-
-
-
-fn parse_functions(program: &str) -> FunctionDeclaration {
-    let program = skip_space(program);
+//parses function. right now takes whole program, starts at 0 and as far as I've gotten which is handling parameters, everything in the block should be reusable from main code
+//
+fn parse_functions(program: String) -> FunctionDeclaration {
+    let program = skip_space(&program);
     //returns position everything after function
     let mat = Regex::new("(function)")
         .unwrap()
@@ -93,10 +196,11 @@ fn parse_functions(program: &str) -> FunctionDeclaration {
         params: params_arr,
         identifier: new_identifier,
     };
-    println!("{:?}", new_func);
+    // println!("{:?}", new_func);
     return new_func;
 }
-
+//these two create_param_array ... functions are for the moment just copies of each other, need to be reworked. 
+//the idea is to return a Params object consisting of vectors of types
 fn create_params_array_declaration(string: &str) -> Result<Params, String> {
     let args_count = string.matches(',').count() + 1;
 
@@ -141,7 +245,7 @@ fn create_params_array_declaration(string: &str) -> Result<Params, String> {
             new_params.bool.push(first.parse::<bool>().unwrap());
         }
         let next_string = temp_string.substring(match_params.end(), temp_string.len() + 1);
-        println!("next string: {}", string);
+        // println!("next string: {}", string);
 
         temp_string = next_string;
     }
@@ -185,7 +289,6 @@ fn create_params_array_expression(string: &str, name: &str) -> Result<Params, St
         if (start_bool && !end_bool) || (!start_bool && end_bool) {
             return Err("Malformed string!".to_string());
         }
-        println!("{}", first);
 
         if first.parse::<f64>().is_ok() {
             new_params.number.push(first.parse::<f64>().unwrap());
@@ -193,18 +296,18 @@ fn create_params_array_expression(string: &str, name: &str) -> Result<Params, St
             new_params.bool.push(first.parse::<bool>().unwrap());
         }
         let next_string = temp_string.substring(match_params.end(), temp_string.len() + 1);
-        println!("next string: {}", string);
+        // println!("next string: {}", string);
 
         temp_string = next_string;
     }
 
     Ok(new_params)
 }
-
+//deletes whitespace
 fn skip_space(slice: &str) -> String {
     slice.split_whitespace().collect()
 }
-
+//opens file, returns in string format
 fn read_file_line_by_line(filepath: &str) -> String {
     let path = Path::new(filepath);
     let file = File::open(path).expect("Cannot open file.txt");
